ac238601b8dd8caaadfe383c4eb10cddd787520a
DenisLuba <delubis@mail.ru>
2022-06-02 14:43:35 +0300

    Made task3012

---
+++ src/com/javarush/task/task30/task3012/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task30.task3012;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.createExpression(1234);
+    }
+    public void createExpression(int number) {
+        System.out.print(number + " = ");
+        StringBuilder ternaryNumber = new StringBuilder();
+        while(number > 0) {
+            ternaryNumber.append(number % 3);
+            if(number % 3 == 2) number = (number / 3) + 1;
+            else number /= 3;
+        }
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        for(char ch : ternaryNumber.toString().toCharArray()) {
+            switch (ch) {
+                case '1' -> result.append(String.format("+ %d ", (int) Math.pow(3, i)));
+                case '2' -> result.append(String.format("- %d ", (int) Math.pow(3, i)));
+            }
+            i++;
+        }
+        System.out.println(result.toString().trim());
+    }
+}
