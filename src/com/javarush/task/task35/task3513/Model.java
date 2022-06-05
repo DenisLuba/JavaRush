ecfca518b15bb25bad2d1e323b2a39c9d0a13b16
DenisLuba <delubis@mail.ru>
2022-06-05 21:25:06 +0300

    Edited a class Model.

--- src/com/javarush/task/task35/task3513/Model.java
+++ src/com/javarush/task/task35/task3513/Model.java
@@ -1,14 +1,40 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
-    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+    private final Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
     public Model() {
-        for (int i = 0; i < FIELD_WIDTH; i++) {
-            for (int j = 0; j < FIELD_WIDTH; j++) {
+        resetGameTiles();
+    }
+
+    public void resetGameTiles() {
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile(); addTile();
+    }
+
+    private void addTile() {
+        List<Tile> tiles = getEmptyTiles();
+        if (!tiles.isEmpty()) {
+            int randomTile = (int) (Math.random() * tiles.size());
+            tiles.get(randomTile).value = Math.random() < 0.9 ? 2 : 4;
+        }
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> tiles = new ArrayList<>();
+        for (Tile[] gameTile : gameTiles) {
+            for (Tile tile : gameTile) {
+                if (tile.isEmpty()) tiles.add(tile);
+            }
+        }
+        return tiles;
     }
 }
