06320e37c023d8598ce721dc65b0592277f6de44
DenisLuba <delubis@mail.ru>
2022-06-10 14:21:52 +0300

    Edited class Model in package task35.task3513

--- src/com/javarush/task/task35/task3513/Model.java
+++ src/com/javarush/task/task35/task3513/Model.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
-    private static final int FIELD_WIDTH = 4;
+    int score = 0;
+
+    int maxTile = 0;
+
+    static final int FIELD_WIDTH = 4;
     private final Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
     public Model() {
@@ -36,5 +40,36 @@ public class Model {
             }
         }
         return tiles;
+    }
+
+    private void compressTiles(Tile[] tiles) {
+        int j = 0;
+        for (int i = 0; i < tiles.length; i++) {
+            if (!tiles[i].isEmpty()) {
+                if (i != j) {
+                    tiles[j] = tiles[i];
+                    tiles[i] = new Tile();
+                }
+                j++;
+            }
+        }
+    }
+
+     private void mergeTiles(Tile[] tiles) {
+        for (int i = 0; i < tiles.length; i++) {
+            if((i + 1) < tiles.length && tiles[i].value == tiles[i + 1].value && !tiles[i].isEmpty()) {
+                tiles[i].value *= 2;
+                tiles[i + 1].value = 0;
+                score += tiles[i].value;
+                maxTile = Math.max(maxTile, tiles[i].value);
+                compressTiles(tiles);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Tile[] tiles = new Tile[] {new Tile(), new Tile(8), new Tile(), new Tile(8)};
+        new Model().compressTiles(tiles);
+        for(Tile title : tiles) System.out.print(title.value + " ");
     }
 }
