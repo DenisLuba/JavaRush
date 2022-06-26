0f7aaae357e1fba7641dc02cd1dbf7a96ad06d21
DenisLuba <delubis@mail.ru>
2022-06-26 16:30:50 +0300

    Counting the implementation of the Controller class

--- src/com/javarush/task/task35/task3513/Controller.java
+++ src/com/javarush/task/task35/task3513/Controller.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
+    private static final int WINNING_TILE = 2048;
     private Model model;
     private View view;
+
+    public Controller(Model model) {
+        this.model = model;
+        view = new View(this);
+    }
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -12,5 +19,22 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameLost = false;
+        view.isGameWon = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch (keyCode) {
+            // 27 - ESCAPE
+            case 27 :
+
+        }
     }
 }
