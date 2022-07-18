734e4feb972b60a2dff3fd695090b2f5b3997f3a
DenisLuba <delubis@mail.ru>
2022-07-18 13:56:37 +0300

    going on task3106

--- src/com/javarush/task/task31/task3106/Solution.java
+++ src/com/javarush/task/task31/task3106/Solution.java
@@ -4,36 +4,34 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.*;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        Path file = Paths.get(args[0]);
-        if (!Files.exists(file.getParent()))
-            Files.createDirectories(file.getParent());
+        Path resultPath = Paths.get(args[0]);
 
-        try (FileOutputStream zipFile = new FileOutputStream(file.getParent().toString() + "file.zip", true);
-        ZipOutputStream zip = new ZipOutputStream(zipFile)) {
+        if (Files.exists(resultPath.getParent()) && !Files.exists(resultPath)) Files.createFile(resultPath);
+        if (!Files.exists(resultPath.getParent())) {
+            Files.createDirectories(resultPath.getParent());
+            Files.createFile(resultPath);
+        }
 
+        TreeSet<String> paths = new TreeSet<>(Arrays.asList(args).subList(1, args.length));
 
-            zip.putNextEntry(new ZipEntry(file.getFileName().toString()));
+        byte[] buffer = new byte[2048];
 
-            Set<String> namesOfFileParts = new TreeSet<>();
-            for (int i = 1; i < args.length; i++)
-                namesOfFileParts.add(args[i]);
+        try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(resultPath.toFile(), true))) {
+            for (String path : paths) {
+                try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(path));
+                     ZipInputStream zipInputStream = new ZipInputStream(bufferedInputStream)) {
 
-            for (String namePart : namesOfFileParts) {
-                try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(namePart))) {
-                    while ((zipInputStream.getNextEntry()) != null) {
-                        Files.copy()
+                    while (zipInputStream.getNextEntry() != null) {
+                        int len;
+                        while ((len = zipInputStream.read(buffer)) > 0)
+                            outputStream.write(buffer, 0, len);
                     }
                 }
-
-
             }
         }
     }
