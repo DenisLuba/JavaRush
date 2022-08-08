fc1f0aa4989c74e45671b9f459abf0879751f9aa
DenisLuba <denislubapershins@gmail.com>
2022-08-08 22:53:47 +0300

    i'm going on to do task3105

--- src/com/javarush/task/task31/task3105/Solution.java
+++ src/com/javarush/task/task31/task3105/Solution.java
@@ -1,10 +1,8 @@
 package com.javarush.task.task31.task3105;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.charset.Charset;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -15,44 +13,63 @@ import java.util.zip.ZipOutputStream;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        String source = "C:\\Users\\support\\Desktop\\Solution\\Folder.zip";
-        Path sourceFolder = Paths.get(source.replace(".zip", ""));
-        String destination = "";
-        ZipFile archive = new ZipFile(source);
+        Path destination = Paths.get("D:\\eng");
+        Path archive = Paths.get("D:\\ENGLISH.zip");
+        decompress(archive, destination);
 
-
-        Enumeration<? extends ZipEntry> entries = archive.entries();
-        while (entries.hasMoreElements()) {
-            ZipEntry entry = entries.nextElement();
-            Path tempFile;
-            Path file = Paths.get(sourceFolder + "\\" + entry.getName());
-            if (!Files.exists(file)) tempFile = entry.isDirectory() ? Files.createDirectory(file) : Files.createFile(file);
-            else tempFile = file;
-            if (entry.isDirectory()) continue;
-
-            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(tempFile.toFile()));
-            zipOutputStream.putNextEntry(entry);
-            copy(archive.getInputStream(entry), zipOutputStream);
-        }
+//        Path sourceFolder = Paths.get(source.replace(".zip", ""));
+//        String destination = "";
+//        ZipFile archive = new ZipFile(source);
+//
+//
+//        Enumeration<? extends ZipEntry> entries = archive.entries();
+//        while (entries.hasMoreElements()) {
+//            ZipEntry entry = entries.nextElement();
+//            Path tempFile;
+//            Path file = Paths.get(sourceFolder + "\\" + entry.getName());
+//            if (!Files.exists(file)) tempFile = entry.isDirectory() ? Files.createDirectory(file) : Files.createFile(file);
+//            else tempFile = file;
+//            if (entry.isDirectory()) continue;
+//
+//            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(tempFile.toFile()));
+//            zipOutputStream.putNextEntry(entry);
+//            copy(archive.getInputStream(entry), zipOutputStream);
+//        }
 
     }
 
-    public static void copy(InputStream inputStream, OutputStream outputStream) {
+    public static void copy(InputStream inputStream, OutputStream outputStream) throws IOException {
         byte[] buffer = new byte[1024 * 1024];
         int bufferSize;
-        try {
-            while ((bufferSize = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bufferSize);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                inputStream.close();
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+
+        while ((bufferSize = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bufferSize);
+        }
+    }
+
+    public static void compress (Path source, Path archive) throws IOException {
+
+    }
+
+    public static void decompress (Path archive, Path destination) throws IOException {
+        try (ZipInputStream input = new ZipInputStream(new BufferedInputStream(new FileInputStream(archive.toFile())), Charset.forName("windows-1251"))) {
+            ZipEntry entry;
+            while ((entry = input.getNextEntry()) != null) {
+                Path filePath = destination.resolve(entry.getName());
+
+                if (entry.isDirectory() && !Files.exists(filePath))
+                    Files.createDirectories(filePath);
+                else if (!entry.isDirectory() && !Files.exists(filePath)) {
+                    if (!Files.exists(filePath.getParent()))
+                        Files.createDirectory(filePath.getParent());
+                    Files.createFile(filePath);
+                    try (BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(filePath.toFile()))) {
+                        copy(input, output);
+                    }
+                }
             }
         }
     }
+
+
 }
