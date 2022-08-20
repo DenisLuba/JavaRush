4acbbc13fdf65655231ac745aba8dd0d3eb38bf6
DenisLuba <Delubis@mail.ru>
2022-08-20 15:47:30 +0300

    task 2028 item 1.

--- src/com/javarush/task/Test.java
+++ src/com/javarush/task/Test.java
@@ -1,36 +1,17 @@
 package com.javarush.task;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class Test {
     public static void main(String[] args) {
-        try(ZipInputStream zin = new ZipInputStream(new FileInputStream("D:\\Folder.zip")))
-        {
-            ZipEntry entry;
-            String name;
-            long size;
-            while((entry=zin.getNextEntry())!=null){
 
-                name = entry.getName(); // получим название файла
-                size=entry.getSize();  // получим его размер в байтах
-                System.out.printf("File name: %s \t File size: %d \n", name, size);
-
-                // распаковка
-                FileOutputStream fout = new FileOutputStream("D:\\" + name);
-                for (int c = zin.read(); c != -1; c = zin.read()) {
-                    fout.write(c);
-                }
-                fout.flush();
-                zin.closeEntry();
-                fout.close();
-            }
-        }
-        catch(Exception ex){
-
-            System.out.println(ex.getMessage());
-        }
     }
 }
