30f3d194f7baa6fd27d9638081b7ee86b8aba8ab
DenisLuba <Delubis@mail.ru>
2022-08-23 20:33:41 +0300

    i finished a task3201

--- src/com/javarush/task/task32/task3201/Solution.java
+++ src/com/javarush/task/task32/task3201/Solution.java
@@ -6,10 +6,12 @@ import java.io.RandomAccessFile;
 public class Solution {
     public static void main(String[] args) {
         try (RandomAccessFile accessFile = new RandomAccessFile(args[0], "rw")) {
-            accessFile.seek(Integer.parseInt(args[1]));
+            System.out.println(accessFile.length());
+            if(accessFile.length() < Long.parseLong(args[1]))
+                accessFile.seek(accessFile.length());
+            else accessFile.seek(Long.parseLong(args[1]));
             accessFile.writeBytes(args[2]);
         } catch (IOException e) {
         }
-
     }
 }
