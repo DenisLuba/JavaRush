136b0ddc8a20497becb6a51defd208757fb959a3
DenisLuba <Delubis@mail.ru>
2022-09-02 19:46:25 +0300

    i finished a task3208

---
+++ src/com/javarush/task/task32/task3208/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task32.task3208;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Solution {
+    public static Registry registry;
+
+    // Pretend we're starting an RMI client as the CLIENT_THREAD thread
+    public static Thread CLIENT_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                for (String bindingName : registry.list()) {
+                    Animal service = (Animal) registry.lookup(bindingName);
+                    service.printName();
+                    service.speak();
+                }
+            } catch (RemoteException | NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    // Pretend we're starting an RMI server as the SERVER_THREAD thread
+    public static Thread SERVER_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                registry = LocateRegistry.createRegistry(2099);
+                Cat cat = new Cat("Cat");
+                Dog dog = new Dog("Dog");
+
+                Remote stubCat = UnicastRemoteObject.exportObject(cat, 0);
+                Remote stubDog = UnicastRemoteObject.exportObject(dog, 0);
+
+                registry.bind("server.cat", stubCat);
+                registry.bind("server.dog", stubDog);
+            } catch (RemoteException | AlreadyBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    public static void main(String[] args) throws InterruptedException {
+       // Starting an RMI server thread
+       SERVER_THREAD.start();
+       Thread.sleep(1000);
+       // Start the client
+        CLIENT_THREAD.start();
+    }
+}
