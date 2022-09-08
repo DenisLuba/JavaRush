afbad8406811fdfc737c7bbd0f3c2c6d9f399cc2
DenisLuba <Delubis@mail.ru>
2022-09-08 13:46:39 +0300

    i finished a task2213 item 14

--- src/com/javarush/task/Test.java
+++ src/com/javarush/task/Test.java
@@ -6,17 +6,100 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Test {
+    static int[][] matrix = {
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1, 1},
+            {1, 0, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1}
+    };
+
+    static void removeFullLines() {
+        int height = matrix.length;
+        int width = matrix[0].length;
+        //Создаем список для хранения линий
+        ArrayList<int[]> lines = new ArrayList<>();
+
+        //Копируем все неполные линии в список.
+        for (int i = 0; i < height; i++) {
+            //подсчитываем количество единиц в строке - просто суммируем все ее значения
+            int count = 0;
+            for (int j = 0; j < width; j++) {
+                count += matrix[i][j];
+            }
+
+            //Если сумма строки не равна ее ширине - добавляем в список
+            if (count != width)
+                lines.add(matrix[i]);
+        }
+
+        //Добавляем недостающие строки в начало списка.
+        while (lines.size() < height) {
+            lines.add(0, new int[width]);
+        }
+
+        //Преобразуем список обратно в матрицу
+        matrix = lines.toArray(new int[height][width]);
+    }
+
+//    public static void removeFullLines() {
+//        int w = matrix[0].length;
+//        boolean isFullLine;
+//        for (int i = 0; i < matrix.length; i++) {
+//            isFullLine = true;
+//            for (int j = 0; j < matrix[i].length; j++)
+//                if (matrix[i][j] == 0) {
+//                    isFullLine = false;
+//                    break;
+//                }
+//            if (isFullLine) matrix[i] = null;
+//        }
+//
+//        if (matrix[0] == null)
+//            matrix[0] = new int[w];
+//        for (int i = 0; i < matrix.length; i++) {
+//
+//            if (matrix[i] == null) {
+//                if (i > 0) {
+//                    int[] rowX = matrix[0].clone();
+//                    int[] rowY;
+//                    for (int j = 1; j <= i; j++) {
+//                        rowY = matrix[j];
+//                        matrix[j] = rowX;
+//                        rowX = rowY;
+//                    }
+//                }
+//                assert matrix[0] != null;
+//                Arrays.fill(matrix[0], 0);
+//            }
+//        }
+//    }
+
+    public static void print() {
+        for (int[] rows : matrix) {
+            for (int cell : rows)
+                System.out.print((cell == 0 ? '.' : 'X') + "  ");
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        byte[] d = new byte[] {90, 71, -47, 42, 46, 63, -97, -20, -14, -39, -70, 31, -39, -127, 82, -21};
-
-        StringBuilder s = new StringBuilder();
-
-        for (byte b : d) s.append(Integer.toHexString(b));
-        System.out.println(s);
+        print();
+        System.out.println('\n');
+        removeFullLines();
+        print();
     }
 }
--- src/com/javarush/task/task22/task2213/Field.java
+++ src/com/javarush/task/task22/task2213/Field.java
@@ -1,14 +1,11 @@
 package com.javarush.task.task22.task2213;
 
-public class Field {
-    private int width, height;
-    private int[][] matrix;
+import java.util.ArrayList;
 
-    public Field(int width, int height) {
-        this.width = width;
-        this.height = height;
-        matrix = new int[height][width];
-    }
+public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
 
     public int getWidth() {
         return width;
@@ -22,23 +19,106 @@ public class Field {
         return matrix;
     }
 
-    public void print() {
-        for (int[] rows : matrix) {
-            for (int cell : rows)
-                System.out.print(cell == 0 ? '.' : 'X');
+    public Field(int width, int height) {
+        this.height = height;
+        this.width = width;
+        matrix = new int[height][width];
+    }
+
+    void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
+        int[][] canvas = new int[height][width];
+
+        //Копируем "матрицу поля" в массив
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                canvas[i][j] = matrix[i][j];
+            }
+        }
+
+        //Копируем фигурку в массив, только непустые клетки
+        int left = Tetris.game.getFigure().getX();
+        int top = Tetris.game.getFigure().getY();
+        int[][] brickMatrix = Tetris.game.getFigure().getMatrix();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (top + i >= height || left + j >= width) continue;
+                if (brickMatrix[i][j] == 1)
+                    canvas[top + i][left + j] = 2;
+            }
+        }
+
+
+        //Выводим "нарисованное" на экран, но начинаем с "границы кадра".
+        System.out.println("---------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int index = canvas[i][j];
+                if (index == 0)
+                    System.out.print(" . ");
+                else if (index == 1)
+                    System.out.print(" X ");
+                else if (index == 2)
+                    System.out.print(" X ");
+                else
+                    System.out.print("???");
+            }
             System.out.println();
         }
+
+
+        System.out.println();
+        System.out.println();
     }
 
-    public void removeFullLines() {
+    void removeFullLines() {
+        //Создаем список для хранения линий
+        ArrayList<int[]> lines = new ArrayList<>();
 
+        //Копируем все неполные линии в список.
+        for (int i = 0; i < height; i++) {
+            //подсчитываем количество единиц в строке - просто суммируем все ее значения
+            int count = 0;
+            for (int j = 0; j < width; j++) {
+                count += matrix[i][j];
+            }
+
+            //Если сумма строки не равна ее ширине - добавляем в список
+            if (count != width)
+                lines.add(matrix[i]);
+        }
+
+        //Добавляем недостающие строки в начало списка.
+        while (lines.size() < height) {
+            lines.add(0, new int[width]);
+        }
+
+        //Преобразуем список обратно в матрицу
+        matrix = lines.toArray(new int[height][width]);
     }
 
+    /**
+     * Метод возвращает значение, которое содержится в матрице с координатами (x,y)
+     * Если координаты за пределами матрицы, метод возвращает null.
+     */
     public Integer getValue(int x, int y) {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
+
         return null;
     }
 
+    /**
+     * Метод устанавливает переданное значение(value) в ячейку матрицы с координатами (x,y)
+     */
     public void setValue(int x, int y, int value) {
-
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            matrix[y][x] = value;
     }
 }
+
+
+
+
