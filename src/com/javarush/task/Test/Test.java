489620031f890fef6d32de1bc1527afbe65046c0
DenisLuba <delubis@mail.ru>
2022-09-19 14:08:56 +0300

    i have done the item 9 of task2515

--- src/com/javarush/task/Test/Test.java
+++ src/com/javarush/task/Test/Test.java
@@ -1,6 +1,9 @@
 package com.javarush.task.Test;
 
 public class Test {
-
+    public static void main(String[] args) {
+        char c = 32;
+        System.out.println("\"" + c + "\"");
+    }
 
 }
--- src/com/javarush/task/task25/task2515/Canvas.java
+++ src/com/javarush/task/task25/task2515/Canvas.java
@@ -37,4 +37,20 @@ public class Canvas {
                 if (matrix[i][j] != 0)
                     setPoint(x + j, y + i, c);
     }
+
+    public void clear() {
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                matrix[i][j] = 32;
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++)
+                System.out.print(matrix[i][j]);
+            System.out.println();
+        }
+
+        System.out.println('\n');
+    }
 }
