fbc0b435f510466cfbfcbd20268eddb1e02ecc77
DenisLuba <Delubis@mail.ru>
2022-09-22 22:19:25 +0300

    i have completed the task3401

--- src/com/javarush/task/task34/task3401/Solution.java
+++ src/com/javarush/task/task34/task3401/Solution.java
@@ -3,7 +3,6 @@ package com.javarush.task.task34.task3401;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-
         System.out.println(solution.fibonacci(9));
         System.out.println(solution.fibonacci(5));
         System.out.println(solution.fibonacci(2));
