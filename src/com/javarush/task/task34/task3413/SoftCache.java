b62510639170dd3faebc624219ce0e87781ed30f
DenisLuba <delubis@mail.ru>
2022-10-06 09:39:45 +0300

    i have finished the task3413

--- src/com/javarush/task/task34/task3413/SoftCache.java
+++ src/com/javarush/task/task34/task3413/SoftCache.java
@@ -1,30 +1,30 @@
 package com.javarush.task.task34.task3413;
 
-import org.glassfish.jaxb.runtime.v2.schemagen.xmlschema.Any;
-
 import java.lang.ref.SoftReference;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class SoftCache {
     private Map<Long, SoftReference<AnyObject>> cacheMap = new ConcurrentHashMap<>();
 
     public AnyObject get(Long key) {
         SoftReference<AnyObject> softReference = cacheMap.get(key);
-
-        // code
+        return softReference == null ? null : softReference.get();
     }
 
-    public AnyObject put(long key, AnyObject value) {
+    public AnyObject put(Long key, AnyObject value) {
         SoftReference<AnyObject> softReference = cacheMap.put(key, new SoftReference<>(value));
-
-        // code
+        if (softReference == null) return null;
+        AnyObject anyObject = softReference.get();
+        softReference.clear();
+        return anyObject;
     }
 
     public AnyObject remove(Long key) {
         SoftReference<AnyObject> softReference = cacheMap.remove(key);
-
-        // code
+        if (softReference == null) return null;
+        AnyObject anyObject = softReference.get();
+        softReference.clear();
+        return anyObject;
     }
 }
