420d76d10e9ebb4896b81f1ea28934b6b25aa0a2
DenisLuba <delubis@mail.ru>
2022-10-07 15:31:52 +0300

    i have finished the task3408

--- src/com/javarush/task/task34/task3408/Cache.java
+++ src/com/javarush/task/task34/task3408/Cache.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task34.task3408;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -8,15 +10,32 @@ public class Cache<K, V> {
 
     public V getByKey(K key, Class<V> clazz) throws Exception {
         //TODO add your code here
-
-        return null;
+        V result = cache.get(key);
+        if (result == null) {
+            try {
+                result = clazz.getConstructor(key.getClass()).newInstance(key);
+            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
+                     InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            cache.put(key, result);
+        }
+        return result;
     }
 
     public boolean put(V obj) {
         //TODO add your code here
-        obj.getKey();
-        cache.put()
-        return false;
+        K key = null;
+        int size = cache.size();
+        try {
+            Method method = obj.getClass().getDeclaredMethod("getKey");
+            method.setAccessible(true);
+            key = (K) method.invoke(obj);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            return false;
+        }
+        cache.put(key, obj);
+        return cache.size() > size;
     }
 
     public int size() {
