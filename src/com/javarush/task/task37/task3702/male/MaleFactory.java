d2e2142fa2443fe6737bc79f6e36ef550a1390db
DenisLuba <Delubis@mail.ru>
2022-10-21 22:16:37 +0300

    i have made the task3702 item 2

---
+++ src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory {
+    public Human getPerson(int age) {
+        return age <= KidBoy.MAX_AGE ? new KidBoy() : age <= TeenBoy.MAX_AGE ? new TeenBoy() : new Man();
+    }
+
+}
