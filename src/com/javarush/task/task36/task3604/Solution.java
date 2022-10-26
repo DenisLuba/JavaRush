2d42a8b4744dbbbab99980e4d0dccd6ca41c2e98
DenisLuba <delubis@mail.ru>
2022-10-26 15:21:53 +0300

    i started task3604

---
+++ src/com/javarush/task/task36/task3604/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task36.task3604;
+
+public class Solution {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(1);
+        tree.insert(100);
+        tree.insert(1000);
+    }
+}
