786e787d91e3bc05627ee7d9dc5a91f67fc0fea5
DenisLuba <Delubis@mail.ru>
2022-11-16 21:29:50 +0300

    finished the task3701

--- src/com/javarush/task/task37/task3701/Solution.java
+++ src/com/javarush/task/task37/task3701/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task37.task3701;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 public class Solution<T> extends ArrayList<T> {
     public static void main(String[] args) {
@@ -19,44 +18,77 @@ public class Solution<T> extends ArrayList<T> {
                 break;
             }
         }
+                for (int i = 4; i < 10_000; i++)
+            list.add(i);
+
+        System.out.println();
+
+        new Thread(() -> {
+            Iterator<Integer> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ignore) { }
+                System.out.println("Thread: " + iterator.next());
+                iterator.remove();
+            }
+        }).start();
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException ignore) { }
+            System.out.println("main " + iterator.next());
+            iterator.remove();
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new RoundIterator<T>(this);
+        return new RoundIterator(this);
     }
 
-    public class RoundIterator<T> implements Iterator<T> {
+    public class RoundIterator implements Iterator<T> {
 
         Solution<T> solution;
         int size;
-        int count;
+        int nextValue, currentValue;
 
         public RoundIterator(Solution<T> solution) {
             this.solution = solution;
             size = solution.size();
-            count = 0;
+            nextValue = 0;
+            currentValue = 0;
         }
 
         @Override
-        public boolean hasNext() {
-            return count < size;
+        public boolean hasNext() throws ConcurrentModificationException {
+            if (isConcurrentModification()) throw  new ConcurrentModificationException();
+
+            return nextValue < size;
         }
 
         @Override
         public T next() {
-            T c = solution.get(count);
-            if (++count == size) count = 0;
-            return c;
+            if (isConcurrentModification()) throw new ConcurrentModificationException();
+
+            T t = solution.get(currentValue = nextValue);
+            if (++nextValue == size) nextValue = 0;
+            return t;
         }
 
         @Override
         public void remove() throws ConcurrentModificationException {
-            try {
-                Iterator.super.remove();
-            } catch(Exception e) {
-                throw new ConcurrentModificationException();
-            }
+            if (isConcurrentModification()) throw new ConcurrentModificationException();
+
+            solution.remove(currentValue);
+            nextValue = nextValue - 1 < 0 ? 0 : --nextValue;
+            size--;
+        }
+
+        private boolean isConcurrentModification() {
+            return size != solution.size();
         }
     }
 }
