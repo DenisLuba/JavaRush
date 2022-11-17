7a6e5c5dd2adfd482be78b6f7e2f84f782b8e9fa
DenisLuba <delubis@mail.ru>
2022-11-17 10:06:04 +0300

    finished the task3703

--- src/com/javarush/task/task33/task3310/strategy/HashBiMapStorageStrategy.java
+++ src/com/javarush/task/task33/task3310/strategy/HashBiMapStorageStrategy.java
@@ -1,37 +1,61 @@
 package com.javarush.task.task33.task3310.strategy;
 
-import com.google.common.collect.HashBiMap;
+//import com.google.common.collect.HashBiMap;
 
 public class HashBiMapStorageStrategy implements StorageStrategy {
-
-    private HashBiMap<Long, String> data;
-
-    public HashBiMapStorageStrategy() {
-        data = HashBiMap.create();
-    }
-
     @Override
     public boolean containsKey(Long key) {
-        return data.containsKey(key);
+        return false;
     }
 
     @Override
     public boolean containsValue(String value) {
-        return data.containsValue(value);
+        return false;
     }
 
     @Override
     public void put(Long key, String value) {
-        data.put(key, value);
+
     }
 
     @Override
     public Long getKey(String value) {
-        return data.inverse().get(value);
+        return null;
     }
 
     @Override
     public String getValue(Long key) {
-        return data.get(key);
+        return null;
     }
+//
+//    private HashBiMap<Long, String> data;
+//
+//    public HashBiMapStorageStrategy() {
+//        data = HashBiMap.create();
+//    }
+//
+//    @Override
+//    public boolean containsKey(Long key) {
+//        return data.containsKey(key);
+//    }
+//
+//    @Override
+//    public boolean containsValue(String value) {
+//        return data.containsValue(value);
+//    }
+//
+//    @Override
+//    public void put(Long key, String value) {
+//        data.put(key, value);
+//    }
+//
+//    @Override
+//    public Long getKey(String value) {
+//        return data.inverse().get(value);
+//    }
+//
+//    @Override
+//    public String getValue(Long key) {
+//        return data.get(key);
+//    }
 }
