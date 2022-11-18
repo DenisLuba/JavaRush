4a941ce254d1d790f52337c2512033cf26d020f2
DenisLuba <delubis@mail.ru>
2022-11-18 11:22:11 +0300

    finished the task3708

--- src/com/javarush/task/task37/task3708/retrievers/CachingProxyRetriever.java
+++ src/com/javarush/task/task37/task3708/retrievers/CachingProxyRetriever.java
@@ -1,15 +1,23 @@
 package com.javarush.task.task37.task3708.retrievers;
 
+import com.javarush.task.task37.task3708.cache.LRUCache;
 import com.javarush.task.task37.task3708.storage.Storage;
 
 public class CachingProxyRetriever implements Retriever {
-    Storage storage;
+    private LRUCache<Long, Object> cache = new LRUCache(16);
+    private OriginalRetriever retriever;
     public CachingProxyRetriever(Storage storage) {
-        this.storage = storage;
+        retriever = new OriginalRetriever(storage);
     }
 
     @Override
     public Object retrieve(long id) {
-        return null;
+        Object result;
+        if ((result = cache.find(id)) != null)
+            return result;
+
+        result = retriever.retrieve(id);
+        cache.set(id, result);
+        return result;
     }
 }
