5e419697439fcbf83d598f80bde5b8ec01d07328
DenisLuba <delubis@mail.ru>
2022-11-26 19:09:06 +0300

    started the task3913

--- src/com/javarush/task/Test/Test.java
+++ src/com/javarush/task/Test/Test.java
@@ -4,7 +4,19 @@ public class Test {
 
 
     public static void main(String[] args) {
-        int i = 0b1 | 0b10;
-        System.out.println(i);
+        int count_0 = 0;
+        int count_1 = 0;
+        int count_2 = 0;
+
+        for(int i = 0; i < 10000; i++) {
+            int random = (int) (Math.random() * 3);
+            switch (random) {
+                case 0 -> count_0++;
+                case 1 -> count_1++;
+                case 2 -> count_2++;
+            }
+        }
+
+        System.out.printf("0 = %d\n1 = %d\n2 = %d", count_0, count_1, count_2);
     }
 }
---
+++ src/com/javarush/task/task39/task3913/LogParser.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task39.task3913;
+
+import com.javarush.task.task39.task3913.query.IPQuery;
+
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.Set;
+
+public class LogParser implements IPQuery {
+    Path logDir;
+
+    public LogParser(Path logDir) {
+        this.logDir = logDir;
+    }
+
+
+    @Override
+    public int getNumberOfUniqueIPs(Date after, Date before) {
+        return getUniqueIPs(after, before).size();
+    }
+
+    @Override
+    public Set<String> getUniqueIPs(Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForUser(String user, Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForEvent(Event event, Date after, Date before) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPsForStatus(Status status, Date after, Date before) {
+        return null;
+    }
+}
