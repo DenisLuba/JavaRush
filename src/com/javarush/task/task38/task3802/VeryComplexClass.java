b53a5c04906856bf617fd7c791c2d848af2c4070
DenisLuba <delubis@mail.ru>
2022-12-14 10:20:46 +0300

    finished the task3802

---
+++ src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task38.task3802;
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws Exception {
+        exception();
+    }
+    public static void main(String[] args) {
+
+    }
+
+    private void exception() throws Exception {
+        throw new Exception();
+    }
+}
