4f95ced73d465aed606d26482997dd0fe1d75a78
DenisLuba <Delubis@mail.ru>
2022-12-16 15:31:12 +0300

    started the task3812

--- src/com/javarush/task/Test/Player.java
+++ src/com/javarush/task/Test/Player.java
@@ -1,41 +1,25 @@
 package com.javarush.task.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Player {
-    public static void main(String[] args) {
+
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int width = in.nextInt();
-        int height = in.nextInt();
-        if (in.hasNextLine()) in.nextLine();
+        String myString = in.nextLine();
 
-        char[][] nodes = new char[height][width];
+        Arrays.asList(myString.toCharArray()).stream().distinct().collect(Collectors.toList());
 
-        for (int i = 0; i < height; i++)
-            nodes[i] = in.nextLine().toCharArray();
 
-        for (int i = 0; i < height; i++)
-            for (int j = 0; j < width; j++) {
-                if (nodes[i][j] == '.') continue;
+        int sum = 0;
+        for (char ch : myString.toUpperCase().toCharArray())
+            sum += ch;
 
-                String node = j + " " + i;
-                String rightNode = "", bottomNode = "";
-
-                for (int k = j + 1; k < width; k++)
-                    if (nodes[i][k] == '0') {
-                        rightNode = k + " " + i;
-                        break;
-                    }
-                if (rightNode.length() == 0) rightNode = "-1 -1";
-
-                for (int k = i + 1; k < height; k++)
-                    if (nodes[k][j] == '0') {
-                        bottomNode = j + " " + k;
-                        break;
-                    }
-                if (bottomNode.length() == 0) bottomNode = "-1 -1";
-
-                System.out.printf("%s %s %s%n", node, rightNode, bottomNode);
-            }
+        char average = (char) (sum / myString.length());
+        System.out.println(average);
     }
 }
---
+++ src/com/javarush/task/task38/task3812/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task38.task3812;
+
+public class Solution {
+    public static void main(String[] args) {
+        printFullyQualifiedNames(Solution.class);
+        printFullyQualifiedNames(SomeTest.class);
+
+        printValues(Solution.class);
+        printValues(SomeTest.class);
+    }
+
+    public static boolean printFullyQualifiedNames(Class c) {
+        return true;
+    }
+
+    public static boolean printValues(Class c) {
+        return true;
+    }
+}
