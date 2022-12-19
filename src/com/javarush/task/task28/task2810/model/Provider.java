1b471053b8199fc295969fc6c06bfa2ba1cd61be
DenisLuba <delubis@mail.ru>
2022-12-19 12:39:17 +0300

    finished item 3 of the task2810

--- src/com/javarush/task/task28/task2810/model/Provider.java
+++ src/com/javarush/task/task28/task2810/model/Provider.java
@@ -1,11 +1,20 @@
 package com.javarush.task.task28.task2810.model;
 
-// this class is a Context class
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
+// this class is a class "Context"
 public class Provider {
     private Strategy strategy;
 
     public Provider(Strategy strategy) {
         this.strategy = strategy;
+    }
+
+    // this method is a method "execute"
+    public List<Vacancy> getJavaVacancies(String searchString) {
+        return null;
     }
 
     public void setStrategy(Strategy strategy) {
