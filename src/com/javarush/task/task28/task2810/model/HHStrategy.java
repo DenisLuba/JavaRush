d40e4d77a6857ae3b5390aff971deaf4e3093e32
DenisLuba <delubis@mail.ru>
2022-12-22 22:22:51 +0300

    finished item 7 of the task2810

--- src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -12,7 +12,11 @@ public class HHStrategy implements Strategy {
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         try {
-            Jsoup.connect(String.format(URL_FORMAT, "NewYork", 1)).get();
+            Jsoup.connect(String.format(URL_FORMAT, "NewYork", 1))
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                    "Chrome/108.0.0.0 Safari/537.36")
+                    .referrer("https://i.hh.ru/css/globals/pages/__globals_4c564e56d7fb5ca268e0ae004f4f2ba9.css")
+                    .get();
         } catch (IOException e) {
             e.printStackTrace();
         }
