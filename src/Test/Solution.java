8271a9609517b552cd0e596b8383dd3e026bf871
DenisLuba <Delubis@mail.ru>
2022-12-25 23:31:54 +0300

    added Kotlin file

---
+++ src/Test/Solution.java
@@ -0,0 +1,15 @@
+package Test;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+class Solution {
+
+}
