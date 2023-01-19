a61127b9d5beeec0b9c4ee30549a0089a9e9c940
DenisLuba <delubis@mail.ru>
2023-01-19 14:26:30 +0300

    started the item 7 of task2613

--- src/com/javarush/task/task26/task2613/CashMachine.java
+++ src/com/javarush/task/task26/task2613/CashMachine.java
@@ -1,16 +1,20 @@
 package com.javarush.task.task26.task2613;
 
+import com.javarush.task.task26.task2613.command.CommandExecutor;
+
 import java.util.Locale;
 
 public class CashMachine {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.ENGLISH);
-        String code = ConsoleHelper.askCurrencyCode();
-        String[] digits = ConsoleHelper.getValidTwoDigits(code);
-        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code);
-        int denomination = Integer.parseInt(digits[0]);
-        int count = Integer.parseInt(digits[1]);
-        manipulator.addAmount(denomination, count);
-        System.out.println(manipulator.getTotalAmount());
+//        Locale.setDefault(Locale.ENGLISH);
+//        String code = ConsoleHelper.askCurrencyCode();
+//        String[] digits = ConsoleHelper.getValidTwoDigits(code);
+//        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code);
+//        int denomination = Integer.parseInt(digits[0]);
+//        int count = Integer.parseInt(digits[1]);
+//        manipulator.addAmount(denomination, count);
+//        System.out.println(manipulator.getTotalAmount());
+
+        CommandExecutor.execute(Operation.INFO);
     }
 }
