aabbab1285c848632f42d314ff78fdd2c7d7fce7
DenisLuba <delubis@mail.ru>
2023-01-27 14:14:38 +0300

    started the item 14 of task2613

--- src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -8,19 +8,24 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LoginCommand implements Command {
-    private String pathToResources = CashMachine.class.getPackage().getName() + ".resources.verifiedCards";
-
-    private ResourceBundle validCreditCards = ResourceBundle.getBundle(pathToResources, Locale.US);
-
+    private String pathToResources = CashMachine.class.getPackage().getName() +".resources.";
+    private String pathToResourceVerifiedCards = pathToResources + "verifiedCards";
+    private String pathToResourceLogin = pathToResources + "login_en";
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(pathToResourceVerifiedCards, Locale.US);
+    private ResourceBundle res = ResourceBundle.getBundle(pathToResourceLogin, Locale.US);
     @Override
     public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage(res.getString("before"));
         while(true) {
-            ConsoleHelper.writeMessage("Please, enter the card number:");
+            ConsoleHelper.writeMessage(res.getString("specify.data"));
             String number = ConsoleHelper.readString();
-            ConsoleHelper.writeMessage("Please, enter the pin code:");
             String pin = ConsoleHelper.readString();
             if (validCreditCards.containsKey(number) &&
-                    validCreditCards.getString(number).equals(pin)) break;
+                    validCreditCards.getString(number).equals(pin)) {
+                String success = String.format(res.getString("success.format"), number);
+                ConsoleHelper.writeMessage(success);
+                break;
+            }
             ConsoleHelper.writeMessage("The data is incorrect. Try again.");
         }
         ConsoleHelper.writeMessage("The data is valid.");
