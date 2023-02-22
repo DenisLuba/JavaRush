8957ad4b4f3b3b4728b1c8421bb84b075a47a81a
DenisLuba <Delubis@mail.ru>
2023-02-23 00:08:01 +0300

    finished the item 5 of the task3410

---
+++ src/com/javarush/task/task34/task3410/model/Box.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+public class Box extends CollisionObject implements Movable {
+
+    public Box(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.ORANGE);
+        int leftBorderX = x - width / 2;
+        int upBorderY = y - height / 2;
+        graphics.fillRect(leftBorderX, upBorderY, width, height);
+    }
+}
--- src/com/javarush/task/task34/task3410/view/Field.java
+++ src/com/javarush/task/task34/task3410/view/Field.java
@@ -13,5 +13,6 @@ public class Field extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+
     }
 }
